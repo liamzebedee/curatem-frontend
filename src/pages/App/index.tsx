9c35da25559a5cd199b2aef63113c5bb2c78b4e5
liamzebedee <liamzebedee@yahoo.com.au>
2021-02-02 13:46:02 +1000

    Flesh out routing and web3

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,4 +1,51 @@
 import React, { StrictMode } from 'react'
-import App from '../../components/App'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import Header from '../../components/Header'
+import Web3ReactManager from '../../components/Web3ReactManager'
+import ViewCommunities from '../ViewCommunities'
 
-export default App
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  overflow-x: hidden;
+`
+
+const HeaderWrapper = styled.div`
+  ${({ theme }) => theme.flexRowNoWrap}
+  width: 100%;
+  justify-content: space-between;
+`
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 100px;
+  align-items: center;
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 10;
+
+  z-index: 1;
+`
+
+export default function App() {
+    return (
+        <AppWrapper>
+            <HeaderWrapper>
+                <Header />
+            </HeaderWrapper>
+            <BodyWrapper>
+                <Web3ReactManager>
+                    <Switch>
+                        <Route exact strict path="/communities" component={ViewCommunities} />
+                        <Redirect to="/communities"/>
+                    </Switch>
+                </Web3ReactManager>
+            </BodyWrapper>
+        </AppWrapper>
+    )
+}
---
+++ src/pages/ViewCommunities/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ViewCommunities() {
+    return <>Communities</>
+}
